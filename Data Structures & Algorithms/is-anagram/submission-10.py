1c8cc911bc6e0a733efe9ad4ccd218a57e247d31
Angadh <angadhv4@gmail.com>
2026-05-29 00:26:00 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s0, s1, s2, s3 = False, False, False, False
+
+        #s0
+        s0 = len(s) == len(t)
+
+        #s1
+        hs = defaultdict(int)
+        ht = defaultdict(int)
+
+        for char in s:
+            hs[char] += 1
+        for char in t:
+            ht[char] += 1
+
+        s1 = hs == ht
+
+        #s2
+        hm = defaultdict(int)
+
+        for char in s:
+            hm[char] += 1
+        for char in t:
+            hm[char] -= 1
+        
+        s2 = all(val == 0 for val in hm.values())
+
+        #s3:
+        s3 = Counter(s) == Counter(t)
+
+        return s0 and s1 and s2 and s3
